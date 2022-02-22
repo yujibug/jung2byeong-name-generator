79f75af6c03c93a27f03b294b98c525eb6861b29
gimyuji476@gmail.com <gimyuji476@gmail.com>
2022-02-22 20:00:40 +0900

    결과 클릭시 복사기능 구현

--- src/Result.js
+++ src/Result.js
@@ -15,6 +15,29 @@ function Result(props) {
     secondName.current.innerHTML = props.secondResult;
   });
 
+  function opyToClipBoard() {
+    let copyText =
+      '크큭..나의 이명은 ' +
+      '<' +
+      props.firstResult +
+      ' ' +
+      props.secondResult +
+      ' ' +
+      props.name +
+      '>' +
+      ' #중2병이름생성기';
+
+    const textArea = document.createElement('textarea');
+    document.body.appendChild(textArea);
+    textArea.value = copyText;
+
+    textArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
+
+    alert('복사했습니다!');
+  }
+
   return (
     <div
       className={
@@ -22,11 +45,13 @@ function Result(props) {
         (props.slideSwitch === true ? ' background-opacity' : '')
       }
     >
-      <p>&#60;니놈의&#62; &#60;이명&#62;은</p>
-      <p>
-        &#60;<span ref={firstName}></span> <span ref={secondName}></span>{' '}
-        <span ref={userName}></span>&#62;
-      </p>
+      <div className='result-textArea' onClick={opyToClipBoard}>
+        <p>&#60;니놈의&#62; &#60;이명&#62;은</p>
+        <p>
+          &#60;<span ref={firstName}></span> <span ref={secondName}></span>{' '}
+          <span ref={userName}></span>&#62;
+        </p>
+      </div>
     </div>
   );
 }
